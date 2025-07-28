a79ec456bf1efed49cf31a0ecc6fd9ab867d3648
gthopkins <54715703+gthopkins@users.noreply.github.com>
2025-07-28 13:08:18 +0100

    Update SuperLearner libraries

--- R/SL_ranger_binom.R
+++ R/SL_ranger_binom.R
@@ -1,19 +1,23 @@
 #' @export
 
 # implement ranger for binomial family
-SL.ranger.binom <- function (Y, X, newX, family, obsWeights, num.trees = 500, mtry = floor(sqrt(ncol(X))), 
-                             write.forest = TRUE, replace = TRUE, sample.fraction = ifelse(replace, 1, 0.632), 
-                             num.threads = 1, verbose = T, ...) {
+SL.ranger.binom <- function (Y, X, newX, obsWeights,
+                             num.trees = 100,
+                             min.node.size = 1,
+                             probability = TRUE,
+                             mtry = floor(sqrt(ncol(X))), write.forest = TRUE,
+                             sample.fraction = ifelse(replace, 1, 0.632), 
+                             replace = TRUE, num.threads = 1, verbose = FALSE, ...) {
   require("ranger")
   Y = factor(Y, levels = c(1, 0))
   if (is.matrix(X)) {
     X = data.frame(X)
   }
   fit <- ranger::ranger(`_Y` ~ ., data = cbind(`_Y` = Y, X), 
-                        num.trees = num.trees, mtry = mtry, min.node.size = 1, 
+                        num.trees = num.trees, mtry = mtry, min.node.size = min.node.size, 
                         replace = replace, sample.fraction = sample.fraction, 
                         case.weights = obsWeights, write.forest = write.forest, 
-                        probability = TRUE, num.threads = num.threads, 
+                        probability = probability, num.threads = num.threads, 
                         verbose = verbose)
   pred <- predict(fit, data = newX)$predictions[, "1"]
   fit <- list(object = fit, verbose = verbose)
--- R/SL_ranger_pois.R
+++ R/SL_ranger_pois.R
@@ -1,18 +1,22 @@
 #' @export
 
 # implement ranger for Poisson family (i.e., not probabilities)
-SL.ranger.pois <- function(Y, X, newX, family, obsWeights, num.trees = 500, mtry = floor(sqrt(ncol(X))), 
-                           write.forest = TRUE, replace = TRUE, sample.fraction = ifelse(replace, 1, 0.632), 
-                           num.threads = 1, verbose = T, ...) {
+SL.ranger.pois <- function(Y, X, newX, obsWeights,
+                           num.trees = 100,
+                           min.node.size = 5,
+                           probability = FALSE,
+                           mtry = floor(sqrt(ncol(X))), write.forest = TRUE,
+                           sample.fraction = ifelse(replace, 1, 0.632), 
+                           replace = TRUE, num.threads = 1, verbose = FALSE, ...) {
   require("ranger")
   if (is.matrix(X)) {
     X = data.frame(X)
   }
   fit <- ranger::ranger(`_Y` ~ ., data = cbind(`_Y` = Y, X), 
-                        num.trees = num.trees, mtry = mtry, min.node.size = 5, 
+                        num.trees = num.trees, mtry = mtry, min.node.size = min.node.size, 
                         replace = replace, sample.fraction = sample.fraction, 
                         case.weights = obsWeights, write.forest = write.forest, 
-                        probability = FALSE, num.threads = num.threads, 
+                        probability = probability, num.threads = num.threads, 
                         verbose = verbose)
   pred <- predict(fit, data = newX)$predictions
   fit <- list(object = fit, verbose = verbose)
